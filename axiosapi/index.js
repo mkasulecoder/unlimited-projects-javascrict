328039fa8db700e5550f2254cdecbbac646b63d8
Mark Kasule <31680529+mkasulecoder@users.noreply.github.com>
2023-03-18 16:40:44 -0400

    Fetching Data from APIs

--- axiosapi/index.js
+++ axiosapi/index.js
@@ -1,28 +1,34 @@
 const axiosRequest = require("axios");
 
-const apiURL = "https://www.boredapi.com/api/activity/";
+const apiUrl = "https://www.boredapi.com/api/activity/";
 
-async function getActivity() {
-  let response = await axiosRequest.get(apiURL);
-  console.log(response);
-}
-getActivity();
+// async function getActivity() {
+//   let response = await axiosRequest.get(apiUrl);
+//   console.log(response);
+// }
 
-async function fetchData() {
+// getActivity();
+
+async function tryGetActivity() {
   try {
-    let response = await axiosRequest.get(apiURL);
+    let response = await axiosRequest.get(apiUrl);
     console.log(response);
   } catch (err) {
     console.log("Error: " + err);
   }
 }
-fetchData();
 
-const arrowFetch = async () => {
-  try {
-    let response = await axiosRequest.get(apiURL);
-    console.log(response);
-  } catch (err) {
-    console.log("Error: " + err);
-  }
-};
+tryGetActivity();
+
+
+
+//arrow functions
+axiosRequest
+  .get(apiUrl)
+  .then((res) => {
+    console.log(res.data);
+  })
+  .catch((err) => {
+    console.log(err.response.data);
+  });
+console.log(arrowFunction);
